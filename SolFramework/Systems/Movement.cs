4410bc4a78ad5cb47e3f42eb2410bf3dad115733
SolarRadiannt <naufal.jf2108@gmail.com>
2026-07-16 20:54:33 +0700

    Movement systems

--- SolFramework/Systems/Movement.cs
+++ SolFramework/Systems/Movement.cs
@@ -4,23 +4,51 @@ using Godot;
 using fennecs;
 
 using SolFramework.Core;
-using SolFramework.Components;
 using SolFramework.Scheduler;
-using System;
+using SolFramework.MoveManager;
 
 
 public partial class Movement : Node, ISystem
 {
 	private static World world = Core.World;
-	public int Priority => SPriority.Highest;
+	private static Stream<MoveDirection, MoveSpeed, MoveVelocity> query_apply_movevel =
+		world.Stream<MoveDirection, MoveSpeed, MoveVelocity>();
+	private static void _SystemApplyMoveVel()
+	{
+		query_apply_movevel.For(
+			static (ref MoveDirection moveDir, ref MoveSpeed speed, ref MoveVelocity moveVel) =>
+			{
+				moveVel.Value = moveDir.Value.Normalized() * speed.Value;
+			});
+	}
+	
+	private static Stream<ECSCharBody2D, MoveDirection, MoveGoal> query_moveto =
+		world.Stream<ECSCharBody2D, MoveDirection, MoveGoal>();
+	private static void _SystemMoveTo()
+	{
+		query_moveto.For(
+			static (ref ECSCharBody2D body, ref MoveDirection moveDir, ref MoveGoal moveGoal) =>
+			{
+				var origin = body.Position;
+				var goal = moveGoal.Value;
+				
+				var direction = (origin - goal).Normalized();
+				moveDir.Value = direction;
+			});
+	}
+	
+	public int Priority => SPriority.Default;
+	public void Process(double delta)
+	{
+		_SystemMoveTo();
+		_SystemApplyMoveVel();
+	}
+	
 	public void Init()
 	{
 		Scheduler.RegisterSystem(this);
 	}
-	public void Process(double delta)
-	{
-		
-	}
+	
 	public override void _Ready()
 	{
 		Init();
