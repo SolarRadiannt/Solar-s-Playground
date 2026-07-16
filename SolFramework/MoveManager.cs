7688308bbff33571f7e801b9733b8a393a0b6a88
SolarRadiannt <naufal.jf2108@gmail.com>
2026-07-16 20:46:13 +0700

    added StopMove and make MoveTo return a bool if it override existing MoveGoal

--- SolFramework/MoveManager.cs
+++ SolFramework/MoveManager.cs
@@ -3,17 +3,32 @@ namespace SolFramework.MoveManager;
 using fennecs;
 using Godot;
 
-
+public record struct MoveSpeed(float Value);
 public record struct MoveDirection(Vector2 Value);
 public record struct MoveGoal(Vector2 Value);
+public record struct MoveVelocity(Vector2 Value);
 
 public static class MoveManager
 {
-	public static void MoveTo(Entity entity, Vector2 goal)
+	public static bool MoveTo(Entity entity, Vector2 goal)
 	{
+		bool overriden = false;
 		if (entity.Has<MoveGoal>())
+		{
 			entity.Remove<MoveGoal>();
+			overriden = true;
+		}
 		
 		entity.Add(new MoveGoal(goal));
+		return overriden;
+	}
+	
+	public static bool StopMove(Entity entity)
+	{
+		if (!entity.Has<MoveGoal>())
+			return false;
+		
+		entity.Remove<MoveGoal>();
+		return true;
 	}
 }
