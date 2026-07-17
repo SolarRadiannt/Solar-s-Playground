f3aa8be95f9e6e6ccf0101998de7fb942e2c15b2
SolarRadiannt <naufal.jf2108@gmail.com>
2026-07-18 00:37:46 +0700

    added GetName method that will get an entity Name or just uses its id

--- SolFramework/Core.cs
+++ SolFramework/Core.cs
@@ -1,8 +1,18 @@
+namespace SolFramework.Core;
 using Godot;
 using fennecs;
 
-namespace SolFramework.Core;
+using SolFramework.Components;
+
 public static class Core
 {
 	public static World World = new World();
+	
+	public static string GetName(Entity entity)
+	{
+		if (entity.Has<Name>())
+			return entity.Ref<Name>().Value;
+		
+		return entity.ToRaw().ToString();
+	}
 }
