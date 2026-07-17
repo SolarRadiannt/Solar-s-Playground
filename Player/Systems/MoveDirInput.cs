c19ec8952a38a543089d82281024e1ce89fa0485
SolarRadiannt <solar.radn@gmail.com>
2026-07-17 11:22:04 +0700

    Player tag

--- Player/Systems/MoveDirInput.cs
+++ Player/Systems/MoveDirInput.cs
@@ -1,8 +1,48 @@
 namespace PlayerManager.Systems;
 using fennecs;
+using PlayerManager.Components;
 using Godot;
+using SolFramework.Scheduler;
+using SolFramework.Core;
+using SolFramework.MoveManager;
 
-public partial class MoveDirInput
+public partial class MoveDirInput : Node, ISystem
 {
-    
+    public int Priority => SPriority.Action - 1;
+    public void Process(double _)
+    {
+        _main();
+    }
+
+    public void Init()
+    {
+        Scheduler.RegisterSystem(this);
+    }
+
+    public override void _Ready()
+    {
+        Init();
+    }
+    private static World world = Core.World;
+    private static Stream<MoveDirection> _arePlayers = world.Query<MoveDirection>()
+        .Has<Player>()
+        .Stream();
+    public void _main()
+    {
+        _arePlayers.For(
+            static (ref MoveDirection moveDir) =>
+            {
+                var dir = Vector2.Zero;
+                if (Input.IsActionPressed("move_up"))
+                    dir += Vector2.Up;
+                if (Input.IsActionPressed("move_down"))
+                    dir += Vector2.Down;
+                if (Input.IsActionPressed("move_left"))
+                    dir += Vector2.Left;
+                if (Input.IsActionPressed("move_right"))
+                    dir += Vector2.Right;
+                
+                moveDir.Value = dir;
+            });
+    }
 };
