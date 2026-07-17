3fa68ea52309f38e8923707693a3426d43a5f05d
SolarRadiannt <solar.radn@gmail.com>
2026-07-17 10:11:22 +0700

    minor changes

--- Main/Main.cs
+++ Main/Main.cs
@@ -1,12 +1,15 @@
+namespace Main;
+
 using Godot;
 using System;
 using SolFramework;
-namespace Main;
+
 public partial class Main : Node
 {
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
