39b167359a18c116fad357f111b8e103c8591e2e
SolarRadiannt <naufal.jf2108@gmail.com>
2026-07-17 23:22:54 +0700

    added destroy and player tag

--- SolFramework/Components.cs
+++ SolFramework/Components.cs
@@ -1,3 +1,11 @@
 namespace SolFramework.Components;
 using Godot;
 public record struct Velocity(Vector2 Value);
+public interface IVelocity
+{
+	public Vector2 Value {get; set;}
+};
+
+public struct Destroy;
+
+public struct Player;
