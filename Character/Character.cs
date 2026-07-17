ee12b886ab61f093179913472417fc536229d80f
SolarRadiannt <solar.radn@gmail.com>
2026-07-17 16:28:35 +0700

    created character

---
+++ Character/Character.cs
@@ -0,0 +1,18 @@
+using Godot;
+using SolFramework.Components;
+using SolFramework.MoveManager;
+using System;
+
+public partial class Character : ECSCharBody2D
+{
+	protected override void OnEntityReady()
+	{
+		entity
+			.Add(new Velocity(Vector2.Zero))
+			.Add(new MoveDirection(Vector2.Zero))
+			.Add(new MoveVelocity(Vector2.Zero))
+			.Add(new MoveSpeed(400));
+		
+		GD.Print("Character spawned!");
+	}
+}
