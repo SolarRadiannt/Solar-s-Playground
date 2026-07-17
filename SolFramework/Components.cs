74f5ccda4bb05cf6a61a28a0e5ac6616ebc54087
SolarRadiannt <naufal.jf2108@gmail.com>
2026-07-18 00:37:58 +0700

    name component

--- SolFramework/Components.cs
+++ SolFramework/Components.cs
@@ -1,11 +1,13 @@
 namespace SolFramework.Components;
 using Godot;
 public record struct Velocity(Vector2 Value);
+public record struct Name(string Value);
+
 public interface IVelocity
 {
 	public Vector2 Value {get; set;}
 };
 
+public struct ChildOf;
 public struct Destroy;
-
 public struct Player;
