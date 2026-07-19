f3f834181a8b0df0a7e0ff5255018b3c96430356
SolarRadiannt <naufal.jf2108@gmail.com>
2026-07-19 19:16:07 +0700

    minor changes

--- Main/Main.cs
+++ Main/Main.cs
@@ -1,16 +1,22 @@
+namespace Root;
+
 using Godot;
 using SolFramework.Scheduler;
 
-public partial class Main : Node
+public partial class Main : Node2D
 {
+	public static Main Instance;
 	private void AddChildBatch(Node[] nodes)
 	{
 		foreach (Node node in nodes)
 			AddChild(node);
 	}
-	
+	private static bool init = false;
 	public override void _EnterTree()
 	{
+		if (init) return;
+		init = true;
+		Instance = this;
 		AddChildBatch(SolFramework.Systems.SystemRegistry.GetAll());
 		AddChildBatch(PlayerManager.Systems.SystemRegistry.GetAll());
 		AddChildBatch(Root.Systems.SystemRegistry.GetAll());
