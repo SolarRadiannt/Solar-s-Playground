9df7389ccbb3bf84ebc40d5e7296398d6c336828
SolarRadiannt <naufal.jf2108@gmail.com>
2026-07-19 13:05:42 +0700

    improved moving check to check the actual last position and the current position delta instead of using Velocity component as if it were collided and you move forward the Velocity is not affected.

--- SolFramework/Systems/MovingChecker.cs
+++ SolFramework/Systems/MovingChecker.cs
@@ -11,21 +11,13 @@ using SolFramework.MoveManager;
 
 public partial class MovingChecker : Node, ISystem
 {
+	private record struct LastPosition(Vector2 Value);
 	private static readonly World world = Core.World;
 	public int Priority => SPriority.Init;
 	public void Process(double delta)
 	{
-		toCheck.For(
-			static(in Entity entity, ref Velocity vel) =>
-			{
-				float speed = vel.Value.Length();
-				bool moving = entity.Has<Moving>();
-				
-				if (!moving && speed > 0.01)
-					entity.Add<Moving>();
-				else if (moving && speed < 0.01)
-					entity.Remove<Moving>();
-			});
+		CheckMoving();
+		UpdateLastPosition();
 	}
 	
 	public void Init()
@@ -34,5 +26,38 @@ public partial class MovingChecker : Node, ISystem
 	}
 	public override void _Ready() => Init();
 	
-	public static readonly Stream<Velocity> toCheck = world.Stream<Velocity>();
+	private static readonly Stream<ECSCharBody2D, LastPosition> toCheck =
+		world.Stream<ECSCharBody2D, LastPosition>();
+	public static void CheckMoving() =>
+		toCheck.For(
+		static (in Entity entity, ref ECSCharBody2D body, ref LastPosition lastPos) =>
+		{
+			var current = body.GlobalPosition;
+			var last = lastPos.Value;
+			
+			var delta = current - last;
+			
+			float speed = delta.Length();
+			bool moving = entity.Has<Moving>();
+				
+			if (!moving && speed > 0.01)
+				entity.Add<Moving>();
+			else if (moving && speed < 0.01)
+				entity.Remove<Moving>();
+		});
+	
+	public static readonly Stream<ECSCharBody2D> toUpdate = world.Stream<ECSCharBody2D>();
+	public static void UpdateLastPosition() =>
+		toUpdate.For(static (in Entity entity, ref ECSCharBody2D body) =>
+		{
+			if (entity.Has<LastPosition>())
+			{
+				entity.Ref<LastPosition>().Value = body.GlobalPosition;
+			}
+			else
+			{
+				entity.Add(new LastPosition(body.GlobalPosition));
+			}
+		});
+	
 }
