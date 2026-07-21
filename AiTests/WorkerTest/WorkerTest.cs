89e3bf7ee375b16f6253e48f280303ce2bafe03e
SolarRadiannt <naufal.jf2108@gmail.com>
2026-07-21 17:24:46 +0700

    htn testing start

---
+++ AiTests/WorkerTest/WorkerTest.cs
@@ -0,0 +1,24 @@
+using Godot;
+using SolFramework.Components;
+using SolFramework.MoveManager;
+using SolFramework.FootstepManager;
+
+public partial class WorkerTest : ECSCharBody2D
+{
+	protected override void OnEntityReady()
+	{
+		entity.Add<Player>();
+		entity.Add<Grounded>();
+		MoveManager.ApplyMovement(entity, 400);
+		FootstepManager.ApplyFootstep(entity, 150);
+		
+		
+	}
+
+	public override void _Ready()
+	{
+		
+		base._Ready();
+	}
+
+}
