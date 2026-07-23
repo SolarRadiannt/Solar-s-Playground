b1ed3b32b6cf8a545c9d1ade9cdac3b2269bc116
SolarRadiannt <solar.radn@gmail.com>
2026-07-23 14:21:57 +0700

    StatValue a class that helps with multiple modifiers on a value.

---
+++ SolFramework/StatValue.cs
@@ -0,0 +1,69 @@
+namespace SolFramework.Tools;
+using System;
+using System.Collections.Generic;
+using Godot;
+
+public struct Modifier
+{
+	public float Additive = 0;
+	public float Multiplier = 1;
+	public float Flat = 0;
+	public Modifier() {}
+}
+
+
+public class StatValue
+{
+	private float _baseValue;
+	private float _value;
+	private readonly Dictionary<string, Modifier> _modifiers = new();
+
+	public Dictionary<string, Modifier>.ValueCollection Modifiers => _modifiers.Values;
+	public float Value => _value;
+	public float BaseValue {
+	get { return _baseValue; }
+	set {
+		_baseValue = value;
+		UpdateCalculation();
+	}}
+
+	public StatValue(float baseValue)
+	{
+		_baseValue = baseValue;
+		_value = baseValue;
+	}
+
+	public StatValue SetModifier(string name, Modifier modifier)
+	{
+		_modifiers[name] = modifier;
+		UpdateCalculation();
+		return this;
+	}
+	public bool HasModifier(string name) =>
+		_modifiers.ContainsKey(name);
+	
+
+	public StatValue RemoveModifier(string name)
+	{
+		_modifiers.Remove(name);
+		UpdateCalculation();
+		return this;
+	}
+
+	private void UpdateCalculation()
+	{
+		float finalAdd = 0;
+		float finalFlat = 0;
+		float finalMult = 1;
+
+		foreach (var (_, modifier) in _modifiers)
+		{
+			finalAdd += modifier.Additive;
+			finalFlat += modifier.Flat;
+			finalMult *= modifier.Multiplier;
+		}
+
+		float finalValue = Mathf.Max(0, _baseValue * (1 + finalAdd) * finalMult + finalFlat);
+		_value = finalValue;
+	}
+}
