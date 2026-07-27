73118e8b61ea61052a7aeddc465ddb11bfb35a98
SolarRadiannt <naufal.jf2108@gmail.com>
2026-07-27 22:37:26 +0700

    dead code cleanup

--- SolFramework/UtilityAI/Components.cs
+++ SolFramework/UtilityAI/Components.cs
@@ -1,7 +1,5 @@
 namespace SolFramework.UtilityAI.Components;
 
-using System.Collections.Generic;
-
 public record struct AgentActions(BaseAction[] Value);
 public struct AgentDisabled;
 public struct AgentIdle;
