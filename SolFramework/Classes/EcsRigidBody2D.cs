ef3409b1e9566edb1b6df32afa3898d276820f99
SolarRadiannt <solar.radn@gmail.com>
2026-07-29 09:35:02 +0700

    added EcsRidgidBody2D

---
+++ SolFramework/Classes/EcsRigidBody2D.cs
@@ -0,0 +1,40 @@
+using fennecs;
+using Godot;
+
+using SolFramework;
+using SolFramework.Components;
+using SolFramework.Tools;
+
+
+[GlobalClass]
+[InspectorColor(InspectColor.Violet)]
+public abstract partial class EcsRigidBody2D : RigidBody2D
+{
+	protected Entity entity;
+	public Entity Entity => entity;
+	protected abstract void OnEntityReady();
+	
+	public override void _EnterTree()
+	{
+		if (!entity)
+			entity = Core.World.Spawn()
+						.Add(new Velocity(Vector2.Zero))
+						.Add(new Name(Name));
+		
+		if (!entity.Has<EcsRigidBody2D>())
+			entity.Add(this);
+		
+		OnEntityReady();
+	}
+	public override void _ExitTree()
+	{
+		if (entity && entity.Has<EcsRigidBody2D>())
+			entity.Remove<EcsRigidBody2D>();
+	}
+	
+	protected override void Dispose(bool disposing)
+	{
+		if (disposing && entity) entity.Despawn();
+		base.Dispose(disposing);
+	}
+}
