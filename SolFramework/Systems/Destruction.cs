d6b740e1876e8b6849fcaf51b90bde676a48245d
SolarRadiannt <naufal.jf2108@gmail.com>
2026-07-30 22:57:04 +0700

    generalized destruction. now they uses Node2DHandle than having to handle multiple EcsWrapper classes

--- SolFramework/Systems/Destruction.cs
+++ SolFramework/Systems/Destruction.cs
@@ -12,17 +12,9 @@ public partial class Destruction : Node, ISystem
 	public int Priority => SPriority.Flush;
 	public void Process(double _)
 	{
-		destroyBody.Raw(static bodies => {
-			foreach (ref var body in bodies.Span)
-				body.QueueFree();
-		});
-		destroyRigidBody.Raw(static rigidBodies => {
-			foreach (ref var body in rigidBodies.Span)
-				body.QueueFree();
-		});
-		destroyNode2d.Raw(static nodes => {
+		destroyHandles.Raw(static nodes => {
 			foreach (ref var node in nodes.Span)
-				node.QueueFree();
+				node.Value.QueueFree();
 		});
 		toDestroy.Despawn();
 	}
@@ -37,19 +29,10 @@ public partial class Destruction : Node, ISystem
 	private static readonly World world = Core.World;
 	private static readonly Stream<Destroy> toDestroy =
 		world.Query<Destroy>()
-			.Not<EcsCharBody2D>()
-			.Not<EcsNode2D>()
+			.Not<Node2DHandle>()
 			.Stream();
-	private static readonly Stream<EcsRigidBody2D> destroyRigidBody=
-		world.Query<EcsRigidBody2D>()
-			.Has<Destroy>()
-			.Stream();
-	private static readonly Stream<EcsCharBody2D> destroyBody =
-		world.Query<EcsCharBody2D>()
-			.Has<Destroy>()
-			.Stream();
-	private static readonly Stream<EcsNode2D> destroyNode2d =
-		world.Query<EcsNode2D>()
+	private static readonly Stream<Node2DHandle> destroyHandles=
+		world.Query<Node2DHandle>()
 			.Has<Destroy>()
 			.Stream();
 }
