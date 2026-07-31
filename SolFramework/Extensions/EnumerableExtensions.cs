79030d7028d8ae220bdef6e921a98f5abac22e86
SolarRadiannt <naufal.jf2108@gmail.com>
2026-07-31 20:00:26 +0700

    optimization on list

--- SolFramework/Extensions/EnumerableExtensions.cs
+++ SolFramework/Extensions/EnumerableExtensions.cs
@@ -6,6 +6,15 @@ public static class EnumerableExtensions
     public static bool TryFirst<T>(this IEnumerable<T> source, out T result)
     {
         if (source == null) throw new ArgumentNullException(nameof(source));
+        
+        if (source is IList<T> list)
+            if (list.Count > 0)
+            {
+                result = list[0];
+                return true;
+            }
+        
+        
         foreach (var item in source)
         {
             result = item;
