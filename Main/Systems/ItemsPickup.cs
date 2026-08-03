484ad8942f3855b2be811863df6c8d9d348f89a2
SolarRadiannt <naufal.jf2108@gmail.com>
2026-08-03 23:21:22 +0700

    minor changes

--- Main/Systems/ItemsPickup.cs
+++ Main/Systems/ItemsPickup.cs
@@ -9,6 +9,10 @@ using SolFramework.Components;
 using SolFramework.Scheduler;
 using SolFramework.Managers;
 using Root.Components;
+using GodotUtilities;
+using SolItems.Components;
+using SolItems.Managers;
+
 
 public partial class ItemsPickup : Node, ISystem
 {
@@ -16,7 +20,23 @@ public partial class ItemsPickup : Node, ISystem
 	public int Priority => SPriority.Action;
 	public void Process(double delta)
 	{
-		PickupHandler();
+		if (!Input.IsActionJustPressed("pickup")) return;
+		
+		GD.Print("pickup clicked");
+		var space_state = MainGame.Instance.GetWorld2D().DirectSpaceState;
+		
+		var query = new PhysicsPointQueryParameters2D();
+		query.Position = MainGame.Instance.GlobalPosition;
+		var result = space_state.IntersectPoint(query)
+		if (result is EcsArea2D area)
+		{
+			var entity = area.Entity;
+			if (entity.HasAll<Pickupable, Item>()) return;
+			if (entity.Has<OwnedBy>(Entity.Any)) return;
+			
+			ItemsManager.Pickup(entity, MainGame.Player.Entity);
+			GD.Print($"{entity.GetName()} has been picked up");
+		}
 	}
 	
 	public void Init()
@@ -24,16 +44,4 @@ public partial class ItemsPickup : Node, ISystem
 		Scheduler.RegisterSystem(this);
 	}
 	public override void _Ready() => Init();
-	
-	private static readonly Stream<EcsArea2D> pickupables =
-		world.Query<EcsArea2D>()
-			.Has<Pickupable>()
-			.Not<OwnedBy>(Entity.Any)
-			.Stream();
-	private static void PickupHandler() =>
-		pickupables.For(static
-		(ref EcsArea2D area) =>
-		{
-			
-		});
 }
